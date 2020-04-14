c9c7cb1e97891669e2ab0c35bd00e6d5fef1fdc1
geovanitec <60049761+geovanitec@users.noreply.github.com>
2020-04-13 21:20:00 -0600

    union de trabajo

--- CONTENIDO_SQL/TABLAS_USUARIOS.sql
+++ CONTENIDO_SQL/TABLAS_USUARIOS.sql
@@ -3,10 +3,12 @@ use NUEVOS_USUARIOS;
 
 create table nuevo_usuario
 (
-
+Id_Usuario int auto_increment ,
 Usuario varchar (60) primary key,
+
 Pass varchar(60) not null,
-Confirmar_Pass varchar (60) not null
+Id_Usuario int primary key
+#Confirmar_Pass varchar (60) not null
 
 )engine=InnoDB Default charset = Latin1 ;
 
