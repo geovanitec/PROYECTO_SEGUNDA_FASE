ff9ac27fecb5f4c0994addfb301acf82e398b463
geovanitec <60049761+geovanitec@users.noreply.github.com>
2020-04-14 18:53:32 -0600

    tablase de usuarios y login admi

--- CONTENIDO_SQL/TABLAS_USUARIOS.sql
+++ CONTENIDO_SQL/TABLAS_USUARIOS.sql
@@ -3,14 +3,18 @@ use NUEVOS_USUARIOS;
 
 create table nuevo_usuario
 (
-Id_Usuario int auto_increment ,
-Usuario varchar (60) primary key,
+Id_Usuario int auto_increment primary key,
+Usuario varchar (60)not null,
 
-Pass varchar(60) not null,
-Id_Usuario int primary key
-#Confirmar_Pass varchar (60) not null
+Pass varchar(60) not null
+
 
 )engine=InnoDB Default charset = Latin1 ;
 
 
+create table usuario_administrador
+(
+Usuario varchar (60) primary key,
+pass varchar (60) not null
 
+)engine=InnoDB Default charset = Latin1 ;
